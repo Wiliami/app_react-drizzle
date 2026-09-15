493ed8b26d2c9593fbb2c4bf665e7bee2e571543
wiliami <wiliaup97@gmail.com>
2026-09-14 23:25:21 -0400

    chore: add schema user

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,4 +5,5 @@ export const usersTable = pgTable("users", {
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+  role: varchar("role", { enum: ["member", "admin"] }).notNull().default("member"),
 });
---
+++ web/src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css'
+import { Users } from './pages/users'
+
+export default function App() {
+  return (
+      <Users />
+  )
+}
